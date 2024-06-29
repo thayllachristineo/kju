d5f22d6d090284daccc867bd9d1262b25b655ae8
thaylla <thayllachristineo@gmail.com>
2024-06-29 15:38:17 -0300

    fix: import lint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
     'eslint-config-prettier',
-    "plugin:react-hooks/recommended"
+    'plugin:react-hooks/recommended',
   ],
   settings: {
     react: {
@@ -20,13 +20,13 @@ module.exports = {
     },
   },
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "react/react-in-jsx-scope": "off",
-    "import/no-named-as-default": 0,
-    "import/no-unresolved": ["error", { "ignore": ["^@/"] }],
-    "react/display-name": "off",
-    "@typescript-eslint/ban-types": "off",
-    "import/named": "off",
-    "react-hooks/exhaustive-deps": "warn"
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': ['error', { ignore: ['~/'] }],
+    'react/display-name': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'import/named': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
