f130c73cb97ac90280c1e6e1e3b45af0fe383d57
thaylla <thayllachristineo@gmail.com>
2024-06-30 13:40:09 -0300

    feat: implements services and loading into dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,61 @@
+import { useEffect, useState } from 'react';
+
+import * as Styled from './styles';
+
 import Collumns from './components/Columns';
-import * as S from './styles';
 import { SearchBar } from './components/Searchbar';
+import {
+  getRegistrationByCpf,
+  getRegistrations,
+} from '~/services/registration';
+import { Registration } from '~/types/registration';
 
 const DashboardPage = () => {
+  const [registrations, setRegistrations] = useState<
+    Registration[] | null
+  >(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const shouldShowSkeleton = registrations === null;
+
+  const fetchRegistrations = async ({
+    showSkeleton = false,
+  } = {}) => {
+    if (showSkeleton) setRegistrations(null);
+    setIsLoading(true);
+    const data = await getRegistrations();
+    setRegistrations(data || []);
+    setIsLoading(false);
+  };
+
+  const fetchRegistrationByCpf = async (cpf: string) => {
+    setRegistrations(null);
+    setIsLoading(true);
+    const data = await getRegistrationByCpf(cpf);
+    setRegistrations(data || []);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchRegistrations();
+  }, []);
+
   return (
-    <S.Container>
-      <SearchBar />
-      <Collumns registrations={[]} />
-    </S.Container>
+    <Styled.Container>
+      <SearchBar
+        fetchRegistrationByCpf={fetchRegistrationByCpf}
+        fetchRegistrations={() =>
+          fetchRegistrations({ showSkeleton: true })
+        }
+      />
+      <Collumns
+        registrations={registrations}
+        fetchRegistrations={fetchRegistrations}
+        isLoading={isLoading}
+        shouldShowSkeleton={shouldShowSkeleton}
+      />
+    </Styled.Container>
   );
 };
 export default DashboardPage;
--- src/pages/Dashboard/styles.tsx
+++ src/pages/Dashboard/styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 24px;
   display: flex;
   flex-direction: column;
+  padding: 24px;
 `;
