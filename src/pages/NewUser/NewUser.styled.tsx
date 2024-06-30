92a26fbb2c46a1251823229500626f518082149d
thaylla <thayllachristineo@gmail.com>
2024-06-30 16:02:14 -0300

    refactor: new user page

---
+++ src/pages/NewUser/NewUser.styled.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.form`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  justify-content: center;
+`;
+
+export const Card = styled.div`
+  border: 2px solid #f0f0f0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 48px;
+  width: 500px;
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-left: 18px;
+  width: 100%;
+`;
