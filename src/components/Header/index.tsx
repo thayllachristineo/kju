a6ade604782ddee7ec93f86394280370b2df1413
thaylla <thayllachristineo@gmail.com>
2024-06-30 11:47:52 -0300

    refactor: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,9 @@
-import styled from 'styled-components';
+import { FC, PropsWithChildren } from 'react';
 
-export const Header = styled.header`
-  background: rgb(255, 117, 0);
-  background: linear-gradient(
-    258deg,
-    rgba(255, 117, 0, 1) 8%,
-    rgba(232, 5, 55, 1) 53%
-  );
-  width: 100%;
-  height: 64px;
-  position: fixed;
-  top: 0;
-  display: flex;
-  align-items: center;
-  padding: 0px 24px;
+import * as Styled from './Header.styled';
 
-  h1 {
-    color: #fff;
-    font-size: 24px;
-  }
-`;
+const Header: FC<PropsWithChildren> = ({ children }) => {
+  return <Styled.Header>{children}</Styled.Header>;
+};
+
+export default Header;
