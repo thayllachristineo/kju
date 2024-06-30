582abe4ed6fbe2ba2bdb5e9ff9ebcb248d672847
thaylla <thayllachristineo@gmail.com>
2024-06-30 11:49:44 -0300

    refactor: textfield component

---
+++ src/components/TextField/TextField.styled.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  background-color: #ffffff;
+  border-radius: 8px;
+  border: 1px solid rgba(36, 28, 21, 0.3);
+  font-size: 16px;
+  font-weight: normal;
+  line-height: 18px;
+  margin-top: 4px;
+  min-height: 36px;
+  padding: 0 8px;
+  transition: all 0.2s ease-in-out 0s;
+  vertical-align: middle;
+  width: 100%;
+  :focus {
+    border: 1px solid #007c89;
+    box-shadow: inset 0 0 0 1px #007c89;
+    outline: none;
+  }
+`;
+
+export const Label = styled.label`
+  color: #24261a;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 18px;
+`;
+
+export const Error = styled.span`
+  color: #ff0000;
+  font-size: 12px;
+  font-weight: normal;
+  line-height: 18px;
+  margin-top: 4px;
+`;
--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,38 +1,25 @@
-import React, { InputHTMLAttributes } from 'react';
-import styled from 'styled-components';
+import { FC, InputHTMLAttributes } from 'react';
 
-export const Input = styled.input`
-  padding: 0 8px;
-  vertical-align: middle;
-  border-radius: 2px;
-  width: 100%;
-  min-height: 36px;
-  background-color: #ffffff;
-  border: 1px solid rgba(36, 28, 21, 0.3);
-  transition: all 0.2s ease-in-out 0s;
-  font-size: 16px;
-  line-height: 18px;
-  font-weight: normal;
-  border-radius: 8px;
-  :focus {
-    outline: none;
-    border: 1px solid #007c89;
-    box-shadow: inset 0 0 0 1px #007c89;
-  }
-`;
+import * as Styled from './TextField.styled';
+
 type Props = {
   label?: string;
   error?: string;
-} & InputHTMLAttributes<any>;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-const TextField = (props: Props) => {
+const TextField: FC<Props> = ({
+  id,
+  label,
+  error,
+  ...props
+}) => {
   return (
     <div>
-      <label htmlFor={props.id}>{props.label}</label>
-      <Input {...props} />
-      <span style={{ fontSize: 12, color: 'red' }}>
-        {props.error}
-      </span>
+      <Styled.Label htmlFor={id}>{label}</Styled.Label>
+      <Styled.Input {...props} />
+      <Styled.Error>{error}</Styled.Error>
     </div>
   );
 };
