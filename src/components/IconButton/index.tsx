72daed80df202442901bc36c0220dd5942cf8fcb
thaylla <thayllachristineo@gmail.com>
2024-06-30 16:00:40 -0300

    feat: add to prop into IconButton

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as Style from './IconButton.styled';
 
@@ -6,10 +7,12 @@ type Props = {
   children?: React.ReactNode;
   onClick?: () => void;
   variant?: 'default' | 'outline';
+  to?: string;
 } & React.HTMLAttributes<HTMLButtonElement>;
 
 const IconButton: FC<Props> = ({
   variant = 'default',
+  to,
   ...props
 }) => {
   const variants = {
@@ -22,7 +25,12 @@ const IconButton: FC<Props> = ({
   }[variant];
 
   return (
-    <Style.IconButton {...variants} {...props}>
+    <Style.IconButton
+      as={to ? Link : 'button'}
+      to={to}
+      {...props}
+      {...variants}
+    >
       {props.children}
     </Style.IconButton>
   );
