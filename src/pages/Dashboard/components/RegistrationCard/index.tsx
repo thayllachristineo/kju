f099399cd90a18947a222201297ff5ed59fc2fa4
thaylla <thayllachristineo@gmail.com>
2024-06-30 13:39:39 -0300

    feat: implements services and loading into registration card

--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -1,45 +1,136 @@
-import { ButtonSmall } from '~/components/Buttons';
-import * as S from './styles';
+import { FC } from 'react';
+import { cpf as cpfValidator } from 'cpf-cnpj-validator';
 import {
   HiOutlineMail,
   HiOutlineUser,
   HiOutlineCalendar,
   HiOutlineTrash,
+  HiOutlineIdentification,
 } from 'react-icons/hi';
 
+import * as Styled from './styles';
+
+import Button from '~/components/Button';
+import IconButton from '~/components/IconButton';
+import {
+  deleteRegistration,
+  setRegistrationStatus,
+} from '~/services/registration';
+import {
+  Registration,
+  RegistrationStatus,
+} from '~/types/registration';
+
 type Props = {
-  data: any;
+  data: Registration;
+  isLoading?: boolean;
+  onMutation: () => void;
 };
 
-const RegistrationCard = (props: Props) => {
-  return (
-    <S.Card>
-      <S.IconAndText>
-        <HiOutlineUser />
-        <h3>{props.data.employeeName}</h3>
-      </S.IconAndText>
-      <S.IconAndText>
-        <HiOutlineMail />
-        <p>{props.data.email}</p>
-      </S.IconAndText>
-      <S.IconAndText>
-        <HiOutlineCalendar />
-        <span>{props.data.admissionDate}</span>
-      </S.IconAndText>
-      <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)">
-          Reprovar
-        </ButtonSmall>
-        <ButtonSmall bgcolor="rgb(155, 229, 155)">
-          Aprovar
-        </ButtonSmall>
-        <ButtonSmall bgcolor="#ff8858">
-          Revisar novamente
-        </ButtonSmall>
+const RegistrationCard: FC<Props> = ({
+  data: {
+    admissionDate,
+    id,
+    email,
+    employeeName,
+    status,
+    cpf,
+  },
+  isLoading,
+  onMutation,
+}) => {
+  const handleReprove = async () => {
+    if (status === RegistrationStatus.REPROVED) return;
 
-        <HiOutlineTrash />
-      </S.Actions>
-    </S.Card>
+    await setRegistrationStatus(
+      id,
+      RegistrationStatus.REPROVED,
+    );
+
+    onMutation?.();
+  };
+
+  const handleApprove = async () => {
+    if (status === RegistrationStatus.APPROVED) return;
+
+    await setRegistrationStatus(
+      id,
+      RegistrationStatus.APPROVED,
+    );
+
+    onMutation?.();
+  };
+
+  const handleReviewAgain = async () => {
+    if (status === RegistrationStatus.REVIEW) return;
+
+    await setRegistrationStatus(
+      id,
+      RegistrationStatus.REVIEW,
+    );
+
+    onMutation?.();
+  };
+
+  const handleDelete = async () => {
+    await deleteRegistration(id);
+
+    onMutation?.();
+  };
+
+  return (
+    <Styled.Card>
+      <Styled.IconAndText>
+        <HiOutlineUser />
+        <h3>{employeeName}</h3>
+      </Styled.IconAndText>
+      <Styled.IconAndText>
+        <HiOutlineMail />
+        <p>{email}</p>
+      </Styled.IconAndText>
+      <Styled.IconAndText>
+        <HiOutlineCalendar />
+        <span>{admissionDate}</span>
+      </Styled.IconAndText>
+      <Styled.IconAndText>
+        <HiOutlineIdentification />
+        <span>{cpfValidator.format(cpf)}</span>
+      </Styled.IconAndText>
+      <Styled.Actions>
+        <div>
+          {status !== RegistrationStatus.REPROVED && (
+            <Button
+              color="pink"
+              onClick={handleReprove}
+              disabled={isLoading}
+            >
+              Reprovar
+            </Button>
+          )}
+          {status !== RegistrationStatus.APPROVED && (
+            <Button
+              color="green"
+              onClick={handleApprove}
+              disabled={isLoading}
+            >
+              Aprovar
+            </Button>
+          )}
+          {status !== RegistrationStatus.REVIEW && (
+            <Button
+              color="orange"
+              onClick={handleReviewAgain}
+              disabled={isLoading}
+            >
+              Revisar novamente
+            </Button>
+          )}
+        </div>
+        <IconButton onClick={handleDelete}>
+          <HiOutlineTrash />
+        </IconButton>
+      </Styled.Actions>
+    </Styled.Card>
   );
 };
 
